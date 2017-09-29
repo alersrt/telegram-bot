11d5c378e0f937b7726ee6919621a682b3871ea4
alersrt <strtmg@yandex.ru>
2017-09-29 22:47:25 +0700

    Create carcass
    - Simple change in JavaDoc of MessageService

--- src/main/java/com/instrumentisto/timebot/service/MessageService.java
+++ src/main/java/com/instrumentisto/timebot/service/MessageService.java
@@ -1,13 +1,14 @@
 package com.instrumentisto.timebot.service;
 
 /**
- * This interface is describing of Service which process some query-message which was got from API.
+ * This interface is describing of Service which process some queries which was got from API.
  */
 public interface MessageService extends Runnable {
 
 
     /**
-     * This method loads list of updates and process all of them.
+     * This method loads list of updates and process all of them by some logic which is set in
+     * implementation.
      */
     void updateService();
 }
