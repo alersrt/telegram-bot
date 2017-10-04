5c6d3b0ecd7356d32d58ab508c5daaaa5929fd3b
alersrt <strtmg@yandex.ru>
2017-10-04 20:21:32 +0700

    Create carcass
    - Add `@Logging` annotation
    - Add `LoggingAnnotationPostProcessor` for `@Logging` annotation

---
+++ src/main/java/com/instrumentisto/timebot/conf/logging/Logging.java
@@ -0,0 +1,16 @@
+package com.instrumentisto.timebot.conf.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for getting of Logger instance for logging application.
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Logging {
+
+    String value() default "";
+}
---
+++ src/main/java/com/instrumentisto/timebot/conf/logging/LoggingAnnotationPostProcessor.java
@@ -0,0 +1,50 @@
+package com.instrumentisto.timebot.conf.logging;
+
+import java.lang.reflect.Field;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link BeanPostProcessor} for {@link Logging} annotation.
+ */
+@Component
+public class LoggingAnnotationPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+        throws BeansException {
+        Class aClass = bean.getClass();
+
+        do {
+            for (Field field : aClass.getDeclaredFields()) {
+                Logging annotation = field.getAnnotation(Logging.class);
+
+                if (annotation != null) {
+                    boolean accessible = field.isAccessible();
+                    field.setAccessible(true);
+                    try {
+                        if (!annotation.value().isEmpty()) {
+                            field.set(bean, LoggerFactory.getLogger(annotation.value()));
+                        } else {
+                            field.set(bean, LoggerFactory.getLogger(aClass));
+                        }
+                    } catch (IllegalAccessException e) {
+                        LoggerFactory.getLogger(this.getClass()).error(e.getMessage(), e);
+                    }
+                    field.setAccessible(accessible);
+                }
+            }
+            aClass = aClass.getSuperclass();
+        } while (aClass != null);
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+        throws BeansException {
+        return bean;
+    }
+}
