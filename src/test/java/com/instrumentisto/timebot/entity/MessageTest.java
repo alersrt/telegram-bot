9a29356ce340788f91394fef0546bab537a21f47
alersrt <strtmg@yandex.ru>
2017-10-05 01:27:07 +0700

    Create base classes
    - Create Message and BaseDTO tests

--- src/test/java/com/instrumentisto/timebot/entity/MessageTest.java
+++ src/test/java/com/instrumentisto/timebot/entity/MessageTest.java
@@ -1,58 +1,87 @@
 package com.instrumentisto.timebot.entity;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * It is Unit-test for {@link Message}.
+ */
 public class MessageTest {
 
-    private Message message1;
-    private Message message2;
-
-    @Before
-    public void setUp() throws Exception {
-        message1 = new Message();
-        message2 = new Message();
-
-        message1.setId(1);
-        message1.setText("It is the simple text.");
-        message1.setChatId("1L");
-
-        message2.setId(2);
-        message2.setText("So, this is some text.");
-        message2.setChatId("2L");
-    }
-
+    /**
+     * Test for {@code id} field getter.
+     */
     @Test
     public void getId() throws Exception {
+
     }
 
+    /**
+     * Test for {@code id} field setter.
+     */
     @Test
     public void setId() throws Exception {
     }
 
+    /**
+     * Test for {@code text} field getter.
+     */
     @Test
     public void getText() throws Exception {
     }
 
+    /**
+     * Test for {@code text} field setter.
+     */
     @Test
     public void setText() throws Exception {
     }
 
+    /**
+     * Test for {@code chatId} field getter.
+     */
     @Test
     public void getChatId() throws Exception {
     }
 
+    /**
+     * Test for {@code chatId} field setter.
+     */
     @Test
     public void setChatId() throws Exception {
     }
 
+    /**
+     * Test for {@code equals()} and {@code hashCode()} methods.
+     */
     @Test
-    public void equals() throws Exception {
+    public void testEqualsHashCode() {
+        Message message1 = new Message();
+        Message message2 = new Message();
+
+        message1.setId(1);
+        message1.setText("The same message");
+        message1.setChatId("1L");
+
+        message2.setId(1);
+        message2.setText("The same message");
+        message2.setChatId("1L");
+
+        Assert.assertTrue("Test equals() for identical objects",
+            message1.equals(message2) && message2.equals(message1));
+        Assert.assertTrue("Test hashCode() for identical objects",
+            message1.hashCode() == message2.hashCode());
+
+        message2.setId(2);
+        message2.setText("Another message");
+        message2.setChatId("2L");
+
+        Assert.assertFalse("Test equals() for different objects",
+            message1.equals(message2) && message2.equals(message1));
+        Assert.assertFalse("Test hashCode() for different objects",
+            message1.hashCode() == message2.hashCode());
+
     }
 
-    @Test
-    public int hashCode() {
-        return 0;
-    }
 
 }
