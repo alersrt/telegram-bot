419025b05a09c097c1faf63dadfa4ed08a4b732f
alersrt <strtmg@yandex.ru>
2017-10-05 22:31:24 +0700

    Create convert utils (!6, #4)
    - Create TelegramUpdateConvertUtil class.

---
+++ src/main/java/com/instrumentisto/timebot/util/TelegramUpdateConverterUtil.java
@@ -0,0 +1,30 @@
+package com.instrumentisto.timebot.util;
+
+import com.instrumentisto.timebot.DTO.BaseDTO;
+import com.instrumentisto.timebot.exception.DTOException;
+import com.pengrad.telegrambot.model.Update;
+
+/**
+ * Class-implementation of {@link ConverterUtil} for {@link Update}.
+ */
+public class TelegramUpdateConverterUtil implements ConverterUtil<Update> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BaseDTO toDTO(Update update) {
+        BaseDTO baseDTO = new BaseDTO();
+        baseDTO.addValueOfField("text", update.message().text());
+        baseDTO.addValueOfField("chatId", update.message().from().id().toString());
+        return baseDTO;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Update fromDTO(BaseDTO baseDto) throws DTOException {
+        throw new DTOException();
+    }
+}
