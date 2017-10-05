06f0b278f5168b4c5b48313cd8377db82d5de5d7
alersrt <strtmg@yandex.ru>
2017-10-05 18:41:41 +0700

    Create interfaces
    - Create repository interface

---
+++ src/main/java/com/instrumentisto/timebot/repository/MessageRepository.java
@@ -0,0 +1,38 @@
+package com.instrumentisto.timebot.repository;
+
+import com.instrumentisto.timebot.entity.Message;
+import java.util.List;
+
+/**
+ * Interface for messages' repository.
+ */
+public interface MessageRepository {
+
+    /**
+     * Searches {@link Message} in repository by ID.
+     *
+     * @param id is id by which you need to find message.
+     * @return {@link Message} value.
+     */
+    Message findById(long id);
+
+    /**
+     * Saves message in to repository.
+     *
+     * @param message is message which you need to save in to repository.
+     */
+    void saveMessage(Message message);
+
+    /**
+     * Gets all messages from repository.
+     *
+     * @return {@link List<Message>} collection.
+     */
+    List<Message> getAllMessages();
+
+    /**
+     * Remove all messages from repository.
+     */
+    void removeAll();
+
+}
